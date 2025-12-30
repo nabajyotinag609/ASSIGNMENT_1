20571f9fcc6172d734c8585481c8d18c44986058
NABAJYOTI <nabajyotinag609@gmail.com>
2025-12-30 23:23:30 +0530

    Initial commit

---
+++ A Personalized Greeting.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your First name: ")
+last_name = input("Enter your Last name: ")
+full_name = first_name + " " + last_name
+print("Hello, ",full_name,"!"," Welcome to the my project.",sep = "")
+
